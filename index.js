8367c43f1917cf357b5e90c9ce4ad6032df141ef
Artem Ivanov <fast0490f@mail.ru>
2018-11-07 18:06:19 +0300

    json parse

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const Plugin = require('./lib/plugin');
 const plugin = new Plugin();
 
 const channels = [
-  { url: 'https://frm.intrahouse.ru/', interval: 2, values: [], statusCode: 200, json: false }
+  { url: 'https://frm.intrahouse.ru/', interval: 2, values: [{ dn: 'lamp1', parse: 'data.a' }, { dn: 'lamp2', parse: 'data.b.b' }], statusCode: 200, json: true }
 ];
 
 function req(url, statusCode) {
   return new Promise((resolve, reject) => {
     request(url, function (error, response, body) {
       if (error === null && (statusCode && response.statusCode === statusCode)) {
-        resolve(body);
+        resolve('{ "a": 1, "b": 2 }');
       } else {
         reject(error || Error(`Response status code no match: ${response.statusCode}`));
       }
@@ -20,9 +20,10 @@ function req(url, statusCode) {
   });
 }
 
-function parserJSON(text) {
-  const json = JSON.parse(text);
-  return json;
+function parserJSON(text, values) {
+  const data = JSON.parse(text);
+  return values
+    .map(item => ({ dn: item.dn, res: item.parse(data) }));
 }
 
 function parserREGEXP(text) {
@@ -31,13 +32,21 @@ function parserREGEXP(text) {
 
 function task(item) {
   req(this.url, this.statusCode)
-    .then(res => this.json ? parserJSON(res) : parserREGEXP(res))
+    .then(res => this.json ? parserJSON(res, this.values) : parserREGEXP(res, this.values))
     .then(values => console.log(values))
     .catch(e => console.log(e.message));
 }
 
 function worker(item) {
-  setInterval(task.bind(item) ,item.interval * 1000)
+  const values = item.json ?
+    item.values
+      .map(i => ({
+        dn: i.dn,
+        parse: new Function('data', `try { return { value: ${i.parse}, status: 1 } } catch (e) { return { error: e.message, status: 0 } }`)
+      }))
+    :
+    item.values;
+  setInterval(task.bind({ ...item, values }) ,item.interval * 1000)
 }
 
 plugin.on('start', () => {
