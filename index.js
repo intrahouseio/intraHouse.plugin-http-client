96f6d78d0ca35aa31eff14c24a2c9f41faf19c0a
Artem Ivanov <fast0490f@mail.ru>
2018-11-09 09:30:35 +0300

    update

--- index.js
+++ index.js
@@ -7,17 +7,26 @@ const plugin = new Plugin();
 const channels = [
   {
     id: '1',
-    url: 'https://frm.intrahouse.ru/',
+    url: 'https://www.starline-online.ru/device?tz=180&_=1541691301544',
     type: 'get',
     interval: 2,
     statusCode: 200,
+    headers: `
+    accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8
+    accept-language: ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7
+    cache-control: max-age=0
+    cookie: _ym_uid=1541054542723769577; _ym_d=1541054542; __utmc=219212379; __utmz=219212379.1541576232.2.2.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); _ym_visorc_20868619=w; _ym_isad=2; __utma=219212379.1086144741.1541054542.1541576232.1541691296.3; __utmt=1; PHPSESSID=472ej88nuj7vdn6ia1aocipmf4; browser_timezone=3; uechat_34028_first_time=1541691298730; uechat_34028_disabled=true; uechat_34028_pages_count=2; __utmb=219212379.3.10.1541691296; lang=ru
+    upgrade-insecure-requests: 1
+    user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36
+    `,
+    body: '',
   },
-  { id: '2', parentid: '1', dn: 'lamp1', type: 'json', json: 'data.a', number: true },
-  { id: '3', parentid: '1', dn: 'lamp2', type: 'text', regexp: 'Всего.сообщений\: <strong>(.*?)<\/strong>', flag: 'gm', rescount: 1, number: true  },
+  { id: '2', parentid: '1', dn: 'lamp1', parseType: 'json', json: 'data.answer.devices[2].car_state', number: false },
+  { id: '3', parentid: '1', dn: 'lamp2', parseType: 'text', regexp: 'Всего.сообщений\: <strong>(.*?)<\/strong>', flag: 'gm', rescount: 1, number: true  },
 ];
 
-function prepareValue(value) {
-  switch (value.type) {
+function prepareChildren(value) {
+  switch (value.parseType) {
     case 'json':
       return ({
         ...value,
@@ -34,28 +43,45 @@ function prepareValue(value) {
   return value;
 }
 
+function prepareParent(value) {
+  const headers = value.headers
+    .split(/\r?\n/)
+    .reduce((l, n) => {
+      const temp = n.split(':');
+      if (temp.length == 2) {
+        return {
+          ...l,
+          [temp[0].replace(/\s/g, '')]: temp[1]
+        }
+      }
+      return l;
+    },{});
+
+  return { ...value, headers };
+}
+
 function prepareData(data) {
   const parent = []
   const children = {}
 
   data
     .forEach(i => {
-      if (i.parentid === undefined) {
-        parent.push(i);
+      if (i.parentid === undefined || i.parentid === false) {
+        parent.push(prepareParent(i));
       } else {
         if (children[i.parentid] === undefined) {
           children[i.parentid] = [];
         }
-        children[i.parentid].push(prepareValue(i));
+        children[i.parentid].push(prepareChildren(i));
       }
     });
 
   return parent.map(i => ({ ...i, values: children[i.id] }));
 }
 
-function req(url, statusCode) {
+function req({ url, type, headers, statusCode }) {
   return new Promise((resolve, reject) => {
-    request(url, function (error, response, body) {
+    request({ uri: url, method: type, headers }, function (error, response, body) {
       if (error === null && (statusCode && response.statusCode === statusCode)) {
         resolve(body);
       } else {
@@ -67,7 +93,7 @@ function req(url, statusCode) {
 
 function parser(text, values) {
   return values
-    .map(value => value.type === 'json' ? parserJSON(text, value) : parserREGEXP(text, value));
+    .map(value => value.parseType === 'json' ? parserJSON(text, value) : parserREGEXP(text, value));
 }
 
 function parserJSON(text, item) {
@@ -91,9 +117,9 @@ function parserREGEXP(text, item) {
 }
 
 function task() {
-  req(this.url, this.statusCode)
+  req(this)
     .then(res => parser(res, this.values))
-    .then(values => console.log(values))
+    .then(values => plugin.debug(values))
     .catch(e => console.log(e.message));
 }
 
